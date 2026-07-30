236642d250fe0c4a301bd28917d4075b51b84f09
Zalinor <zalinordreamer@outlook.com>
2026-07-30 17:11:53 +0300

    add task creation and split task into columns by status

--- src/App.jsx
+++ src/App.jsx
@@ -11,36 +11,65 @@ function TaskCard({ text, onDelete }) {
     </div>;
 }
 
-function App() {
+function Column({title, tasks, onDelete}) {
+  return(
+    <div className="column">
+      <h2>{title}</h2>
+      <div className="cards">
+        {tasks.map((task) => (
+          <TaskCard
+            key={task.id}
+            text={task.text}
+            onDelete={() => onDelete(task.id)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
 
-  const [tasks, setTasks] = useState([
-  {id: 1, text: "Купить ведро котят"},
-  {id: 2, text: "Купить ведро щенят"}
-]);
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  
+  const todoTasks = tasks.filter((task) => task.status === "todo");
+  const inProgressTasks = tasks.filter((task) => task.status === "in-progress");
+  const doneTasks = tasks.filter((task) => task.status === "done");
+
+
+
+  const handleAdd = () => {
+    const text = inputValue.trim();
+    if (text === "") return;
+    
+    const newTask = {
+      id: Date.now(),
+      text: text,
+      status: "todo"
+    };
+
+    setTasks([...tasks, newTask]);
+    setInputValue("");
+  };
 
   const handleDelete = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
   }
-  // const [count, setCount] = useState(0)
   return (
     <>
     <div>
+      <input 
+        type="text"
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)} 
+      />
+      <button onClick={handleAdd}>Добавить</button>
+    </div>
 
-      {tasks.map((task) => (
-        <TaskCard
-        key={task.id}
-        text={task.text}
-        onDelete={() => handleDelete(task.id)}
-        />
-      ))}
-      
-        {/* <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button> */}
+    <div className="board">
+      <Column title="To Do" tasks={todoTasks} onDelete={handleDelete}/>
+      <Column title="In Progress" tasks={inProgressTasks} onDelete={handleDelete}/>
+      <Column title="Done" tasks={doneTasks} onDelete={handleDelete}/>
     </div>
     </>
   )
